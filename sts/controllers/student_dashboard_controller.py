378f5aec9b3e7bc3aca50a79104303fde187046a
Md-Sohel-Rana-Ovi <mds281399@gmail.com>
2022-11-05 11:54:27 +0600

    student personal info show using api

--- sts/controllers/student_dashboard_controller.py
+++ sts/controllers/student_dashboard_controller.py
@@ -11,9 +11,9 @@ _logger = logging.getLogger(__name__)
 class StsDashboard(http.Controller):
     @http.route('/dashboard', auth='public', website=True)
     def dashboard(self, **kw):
-        print(kw)
-        print(kw)
-        print(kw)
+        # print(kw)
+        # print(kw)
+        # print(kw)
         student_info = None
         if kw.get('student_id',False):
             student_info = self.get_student_info(kw['student_id'])
@@ -45,10 +45,9 @@ class StsDashboard(http.Controller):
 class StsStudentInfo(http.Controller):
     @http.route('/personal/info', auth='public', website=True)
     def pesonal_info(self, **kw):
-        print(kw)
-        print(kw)
-        print(kw)
-        print(kw)
+        # print(kw)
+        # print(kw)
+        # user = requests.Session()
         student_info = None
         if kw.get('student_id',False):
             student_info = self.get_student_info(kw['student_id'])
